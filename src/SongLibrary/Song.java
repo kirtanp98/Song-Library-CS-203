6433559b1a5ddfeb83c0dacc986313191bea3966
Kirtan Patel <kirtanpatl@gmail.com>
2019-09-18 21:00:14 -0400

    Add Song class

---
+++ src/SongLibrary/Song.java
@@ -0,0 +1,30 @@
+package SongLibrary;
+
+public class Song {
+
+    public String name;
+    public String artist;
+    public String album;
+    public int year;
+
+    public Song(){
+        this.name="";
+        this.artist="";
+        this.album="";
+        this.year=0;
+    }
+
+    public Song(String name, String artist, String album, int year){
+        this.name=name;
+        this.artist=artist;
+        this.album=album;
+        this.year=year;
+    }
+
+    public void editSong(String name, String artist, String album, int year){
+        this.name=name;
+        this.artist=artist;
+        this.album=album;
+        this.year=year;
+    }
+}
--- src/SongLibrary/SongLib.java
+++ src/SongLibrary/SongLib.java
@@ -11,7 +11,7 @@ public class SongLib extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Song Library");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
