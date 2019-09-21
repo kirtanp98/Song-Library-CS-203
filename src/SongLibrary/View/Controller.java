9c5ea62866b58416becf481bd19ecf2efa1e942f
Kirtan Patel <kirtanpatl@gmail.com>
2019-09-21 14:17:15 -0400

    Add methods for buttons

--- src/SongLibrary/View/Controller.java
+++ src/SongLibrary/View/Controller.java
@@ -1,11 +1,18 @@
 package SongLibrary.View;
 
 import SongLibrary.Model.Song;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Controller {
     @FXML ListView<Song> songList;
@@ -20,4 +27,18 @@ public class Controller {
     @FXML TextField artistField;
     @FXML TextField albumField;
     @FXML TextField yearField;
+
+    private ObservableList<String> obsList;
+
+    public void add(ActionEvent e) {
+        System.out.println("add");
+  }
+
+    public void edit(ActionEvent e) {
+        System.out.println("edit");
+    }
+
+    public void delete(ActionEvent e) {
+        System.out.println("delete");
+    }
 }
