a5dcb0018cda0ee65d6a80aff44e4cfbfff8a876
Kirtan Patel <kirtanpatl@gmail.com>
2019-09-21 13:17:03 -0400

    Add FML properties to the controller

--- src/SongLibrary/View/Controller.java
+++ src/SongLibrary/View/Controller.java
@@ -1,4 +1,23 @@
 package SongLibrary.View;
 
+import SongLibrary.Model.Song;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+    @FXML ListView<Song> songList;
+    @FXML Button add;
+    @FXML Button edit;
+    @FXML Button delete;
+    @FXML Text name;
+    @FXML Text artist;
+    @FXML Text album;
+    @FXML Text year;
+    @FXML TextField nameField;
+    @FXML TextField artistField;
+    @FXML TextField albumField;
+    @FXML TextField yearField;
 }
